6f3dbe70ef5ad62fd5c617fac8217b87bba2fb38
artiwok <artemolga@ukr.net>
2020-11-12 01:57:52 +0200

    Add project files.

---
+++ LoopFactorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopFactorial
+{
+    class Program
+    {
+        static int LoopFactorial(int num)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+
+            return result;
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            LoopFactorial(7); 
+        }
+    }
+}
